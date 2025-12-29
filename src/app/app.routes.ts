a63a665087f20993d1838b293f0a79f4663ed636
Madhu <medarama86@gmail.com>
2025-12-29 19:46:49 +0000

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      {
+    path: '', 
+    component: Home, // This will show when you click "Home"
+    pathMatch: 'full'
+  },
+    {
+    path: 'profile',
+    loadComponent: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './ProfileView'
+      }).then(m => m.ProfileView)
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('host-webpack');
+  isRemoteActive = false;
+
+  constructor(private router: Router) {
+    // Listen to route changes
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      // If the URL contains 'profile', we are in the remote module
+      this.isRemoteActive = event.url.includes('/profile');
+    });
+  }
 }
